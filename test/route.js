d3762a384d2aaa1177c3bd61149bb9152909906a
alex94puchades <alex94puchades@gmail.com>
2015-05-10 23:28:37 +0200

    Implemented basic test coverage

--- test/route.js
+++ test/route.js
@@ -1,27 +1,97 @@
 var rbacAuth = {};
 rbacAuth.mongoose = require('../');
+var Route = rbacAuth.mongoose.Route;
 
 var should = require('should');
 var mongoose = require('mongoose');
-var mockgoose = require('mockgoose');
-mockgoose(mongoose);
+var ObjectId = mongoose.Schema.ObjectId;
+var Schema = mongoose.Schema;
 
-var User = require('./models/users');
+mongoose.connect('mongodb://localhost/test');
 
 describe('Route', function() {
-	var route;
-	before(function() {
-		var credRoute = rbacAuth.mongoose.start({ user: String, pass: String });
-		route = credRoute.field('user').rel('link', 'name').gives(User);
+	describe('#routeFrom', function() {
+		describe('field', function() {
+			var route;
+			before(function() {
+				route = new Route({ field: String }).field('field');
+			});
+
+			it('should invoke callback with field value', function(done) {
+				route.routeFrom({ field: 'value' }, function(err, fieldValue) {
+					if (err)
+						return done(err);
+					fieldValue.should.equal('value');
+					return done();
+				});
+			});
+
+			it('should invoke callback with null otherwise', function(done) {
+				route.routeFrom({}, function(err, fieldValue) {
+					if (err)
+						return done(err);
+					(!!fieldValue).should.be.false;
+					return done();
+				});
+			});
+		});
+
+		describe('linkWith', function(done) {
+			var Foo, foo, route;
+			before(function(done) {
+				var fromRoute = new Route({ link: String });
+				var fooSchema = new Schema({ linked: String });
+				Foo = mongoose.model('Foo', fooSchema);
+				route = Route.newFrom(fromRoute).field('link').linkWith('linked').gives(Foo);
+				foo = new Foo({ linked: 'value' });
+				foo.save(done);
+			});
+
+			after(function(done) {
+				Foo.remove(done);
+			});
+
+			it('should invoke callback with correct object', function(done) {
+				route.routeFrom({ link: 'value' }, function(err, foo) {
+					if (err)
+						return done(err);
+					(!!foo).should.be.true;
+					foo.linked.should.equal('value');
+					return done();
+				});
+			});
+
+			it('should invoke callback with null otherwise', function(done) {
+				route.routeFrom({ link: 'invalid' }, function(err, foo) {
+					if (err)
+						return done(err);
+					(!!foo).should.be.false;
+					return done();
+				});
+			});
+		});
 	});
 
-	describe('#routeFrom', function() {
-		it('should return the correct object', function(done) {
-			route.routeFrom({ user: 'guest' }, function(err, user) {
+	describe('#checkRoute', function() {
+		var route;
+		before(function() {
+			route = new Route({ field: [String] }).field('field').gives([String]);
+		});
+
+		it('should invoke callback with true if field contains value', function(done) {
+			route.checkRoute({ field: ['foo', 'bar'] }, 'foo', function(err, hasValue) {
 				if (err)
 					return done(err);
-				(user !== null).should.be.true;
-				user.name.should.equal('guest');
+				hasValue.should.be.true;
+				return done();
+			});
+		});
+
+		it('should invoke callback with false otherwise', function(done) {
+			route.checkRoute({ field: ['foo', 'bar'] }, 'invalid', function(err, hasValue) {
+				if (err)
+					return done(err);
+				hasValue.should.be.false;
 				return done();
 			});
 		});
